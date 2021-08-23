8ee4cd059145c789b942cbb0a0fe0bb9abc3fc5a
jun <jun3518@mindslab.ai>
2021-08-23 09:27:04 +0900

    Now it works for all version of torch

--- filter.py
+++ filter.py
@@ -3,6 +3,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 import math
 
+if 'sinc' in dir(torch):
+    sinc = torch.sinc
+else:
+# This code is adopted from adefossez's julius.core.sinc
+# https://adefossez.github.io/julius/julius/core.html
+    def sinc(x: torch.Tensor):
+        """
+        Implementation of sinc, i.e. sin(pi * x) / (pi * x)
+        __Warning__: Different to julius.sinc, the input is multiplied by `pi`!
+        """
+        return torch.where(x == 0,
+                           torch.tensor(1., device=x.device, dtype=x.dtype),
+                           torch.sin(math.pi * x) / math.pi / x)
+
 
 # This code is adopted from adefossez's julius.lowpass.LowPassFilters
 # https://adefossez.github.io/julius/julius/lowpass.html
@@ -16,10 +30,8 @@ class LowPassFilter1d(nn.Module):
         super().__init__()
         if cutoff < -0.:
             raise ValueError("Minimum cutoff must be larger than zero.")
-
-
-#        if cutoff > 0.5:
-#            raise ValueError("A cutoff above 0.5 does not make sense.")
+        if cutoff > 0.5:
+            raise ValueError("A cutoff above 0.5 does not make sense.")
         self.stride = stride
         self.pad = pad
         self.kernel_size = kernel_size
@@ -37,11 +49,11 @@ class LowPassFilter1d(nn.Module):
             beta = 0.
         window = torch.kaiser_window(kernel_size, beta=beta, periodic=False)
         #ratio = 0.5/cutroff
-        time = (torch.arange(-self.half_size, self.half_size) + 0.5) 
+        time = (torch.arange(-self.half_size, self.half_size) + 0.5)
         if cutoff == 0:
             filter_ = torch.zeros_like(time)
         else:
-            filter_ = 2 * cutoff * window * torch.sinc(2 * cutoff * time)
+            filter_ = 2 * cutoff * window * sinc(2 * cutoff * time)
             # Normalize filter to have sum = 1, otherwise we will have a small leakage
             # of the constant component in the input signal.
             filter_ /= filter_.sum()
@@ -73,10 +85,8 @@ class LowPassFilter2d(nn.Module):
         super().__init__()
         if cutoff < -0.:
             raise ValueError("Minimum cutoff must be larger than zero.")
-
-
-#        if cutoff > 0.5:
-#            raise ValueError("A cutoff above 0.5 does not make sense.")
+        if cutoff > 0.5:
+            raise ValueError("A cutoff above 0.5 does not make sense.")
         self.stride = stride
         self.pad = pad
         self.kernel_size = kernel_size
@@ -97,7 +107,7 @@ class LowPassFilter2d(nn.Module):
         time = (torch.stack(torch.meshgrid(
             torch.arange(-self.half_size, self.half_size) + 0.5,
             torch.arange(-self.half_size, self.half_size) + 0.5),
-                            dim=-1))  
+                            dim=-1))
         time = torch.norm(time, dim=-1)
         #rotation equivariant window
         window = torch.i0(
@@ -110,7 +120,7 @@ class LowPassFilter2d(nn.Module):
         if cutoff == 0:
             filter_ = torch.zeros_like(time)
         else:
-            filter_ = 2 * cutoff * window * torch.sinc(2 * cutoff * time)
+            filter_ = 2 * cutoff * window * sinc(2 * cutoff * time)
             # Normalize filter to have sum = 1, otherwise we will have a small leakage
             # of the constant component in the input signal.
             filter_ /= filter_.sum()
@@ -125,7 +135,7 @@ class LowPassFilter2d(nn.Module):
             x = F.pad(x, (self.half_size, self.half_size, self.half_size,
                           self.half_size),
                       mode='reflect')
-                      #value=0)
+            #value=0)
 
         out = F.conv2d(x, self.filter, stride=self.stride)[..., :-1, :-1]
         new_shape = shape[:-2] + list(out.shape)[-2:]
